336458c149afe81e0864005e7f286b4b9445b272
LittleDiegoGo <dbg2102d@gmail.com>
2024-11-04 20:55:46 -0600

    Integración de proyecto API a repositorio

---
+++ GestorEmpleados/Controllers/CompraController.cs
@@ -0,0 +1,52 @@
+﻿using GestorEmpleados.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiWebAPI.Data;
+using MiWebAPI.Models;
+
+namespace MiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompraController : ControllerBase
+    {
+        private readonly CompraData _compraData;
+        public CompraController(CompraData compraData)
+        {
+            _compraData = compraData;
+        }
+
+        [HttpPost]
+        [Route("GetCompra")]
+        public async Task<IActionResult> Lista([FromBody] String filtro)
+        {
+            List<Compra> Lista = await _compraData.GetCompra(filtro);
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+
+        [HttpPost]
+        [Route("AddCompra")]
+        public async Task<IActionResult> AddEmpleado([FromBody] Compra objeto)
+        {
+            var respuesta = await _compraData.AddCompra(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("UpdateCompra")]
+        public async Task<IActionResult> UpdateEmpleado([FromBody] Compra objeto)
+        {
+            var respuesta = await _compraData.UpdateCompra(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("DeleteCompra")]
+        public async Task<IActionResult> DeleteEmpleado(int id)
+        {
+            var respuesta = await _compraData.DeleteCompra(id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+    }
+}
+
---
+++ GestorEmpleados/Models/Compra.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorEmpleados.API.Models
+{
+    public class Compra
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Total { get; set; }
+
+        public decimal Subtotal { get; set; }
+
+        public string Cliente { get; set; }
+    }
+}
